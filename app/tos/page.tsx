2c9b386a4273646450fad3835d1d5157e6178b84
Marqe <marqe.mywords@gmail.com>
2024-11-13 17:14:23 +0100

    add page export TOS

---
+++ app/tos/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TosPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Terms of Service</h1>
+            <p>Here are the terms of service...</p>
+        </div>
+    );
+};
+
+export default TosPage;
